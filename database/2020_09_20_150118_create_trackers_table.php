9c51aeaedaa384483c64f6e66a38000574847c17
Reza Amini <rezaaminiroyal@gmail.com>
2021-01-03 22:37:27 +0330

    add migrations

---
+++ database/2020_09_20_150118_create_trackers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrackersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trackers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('trackable_id');
+            $table->string('trackable_type');
+            $table->text('request');
+            $table->string('ip');
+            $table->string('country')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trackers');
+    }
+}
